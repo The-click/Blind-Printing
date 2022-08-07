09807a16a715917eba38dca41726851215cec725
The-click <0leg.19-94@mail.ru>
2022-08-07 04:39:31 +0300

    Prototype project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tapingText, setTapingText] = useState('');
+  const [text, setText] = useState('Lalo da da net net dlkfj lkj ldkjal kjdlfkj a;lkj ;ldakj ;lkj la;kj m l;akj ljk ;oj oaijd ada ;l lakdj ;lakjd lkjd alkjd lkj alkj ;lak j lkja l;kj  j j jj j jjjjjjjjjj ladkj ;lakjf l;kjf al;kjd l;kjkl;j;lkjl;k j  lakjdl; kjafl; kja;l kjd lkj')
+  const [showText, setShowText] = useState(text.split('').map(el => (<span>{el}</span>)));
+  const [isErorr, setIsErorr] = useState(false);
+  function changeHandler(e){
+    let printText = e.target.value;
+    let lenText = printText.length
+    if(text.substring(0, lenText) === printText){
+      setIsErorr(false);
+      setTapingText(e.target.value);
+      setShowText(text.split('').map((el, i) => {
+        if (i < lenText){
+          return (<span className='green'>{el}</span>)
+
+        }
+        return (<span>{el}</span>)}))
+      
+     
+    }else{
+      setIsErorr(true)
+      
+    }
+
+  }
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='text'>
+        {showText}
+      </div>
+      <input onChange={changeHandler} value={tapingText} className={isErorr ? 'red' : ''}></input>
+   
     </div>
   );
 }
