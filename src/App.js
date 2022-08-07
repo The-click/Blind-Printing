74d05f86538cf4d577e9b8bd249230e0a4633332
The-click <0leg.19-94@mail.ru>
2022-08-08 01:51:02 +0300

    Add effect print

--- src/App.js
+++ src/App.js
@@ -4,33 +4,46 @@ import './App.css';
 
 function App() {
   const [tapingText, setTapingText] = useState('');
-  const [text, setText] = useState('Lalo da da net net dlkfj lkj ldkjal kjdlfkj a;lkj ;ldakj ;lkj la;kj m l;akj ljk ;oj oaijd ada ;l lakdj ;lakjd lkjd alkjd lkj alkj ;lak j lkja l;kj  j j jj j jjjjjjjjjj ladkj ;lakjf l;kjf al;kjd l;kjkl;j;lkjl;k j  lakjdl; kjafl; kja;l kjd lkj')
-  const [showText, setShowText] = useState(text.split('').map(el => (<span>{el}</span>)));
+  const [text, setText] = useState("Tail dolor nisi, culpa proident in turducken ea pork chop adipisicing.  Sint boudin ut burgdoggen sausage deserunt eu beef ribs.  Enim aliqua ullamco quis.  Eu bacon chicken boudin, sunt in kevin ut pariatur excepteur.  Salami cupidatat ad, rump pork belly filet mignon pork loin spare ribs proident velit esse strip steak buffalo boudin.  Ex ad eiusmod, cupidatat pariatur incididunt hamburger.")
+  const [showText, setShowText] = useState(text.split('').map((el, i) => {
+    if (i === 0){
+      return (<span className='letter__green'>{el}</span>)
+    }else{
+      return (<span className='letter'>{el}</span>)
+    }
+  }));
   const [isErorr, setIsErorr] = useState(false);
   function changeHandler(e){
     let printText = e.target.value;
-    let lenText = printText.length
+    let lenText = printText.length;
     if(text.substring(0, lenText) === printText){
       setIsErorr(false);
       setTapingText(e.target.value);
       setShowText(text.split('').map((el, i) => {
         if (i < lenText){
-          return (<span className='green'>{el}</span>)
-
+          return (<span className='letter__print'>{el}</span>)
         }
-        return (<span>{el}</span>)}))  
+        if (i == lenText){
+          return (<span className='letter__green'>{el}</span>)
+        }
+        return (<span className='letter'>{el}</span>)}))  
     }else{
-      setIsErorr(true)  
+      setIsErorr(true);
+      setShowText(prevText => {
+        prevText[lenText - 1] = (<span className='letter__green error'>{text[lenText - 1]}</span>)
+        return prevText
+    })
+
     }
   }
  
 
   return (
     <div className="App">
-      <div className='text'>
+      <div className='container__text' >
         {showText}
       </div>
-      <input onChange={changeHandler} value={tapingText} className={isErorr ? 'red' : ''}></input>
+      <input onChange={changeHandler} value={tapingText} className={isErorr ? 'red input' : 'input'} ></input>
    
     </div>
   );
