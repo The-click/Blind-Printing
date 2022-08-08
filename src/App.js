5a467b0113f3796821fed70c0ec529c8512c4113
The-click <0leg.19-94@mail.ru>
2022-08-08 20:09:32 +0300

    Add effect print

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 
 function App() {
   const [tapingText, setTapingText] = useState('');
-  const [text, setText] = useState("Tail dolor nisi, culpa proident in turducken ea pork chop adipisicing.  Sint boudin ut burgdoggen sausage deserunt eu beef ribs.  Enim aliqua ullamco quis.  Eu bacon chicken boudin, sunt in kevin ut pariatur excepteur.  Salami cupidatat ad, rump pork belly filet mignon pork loin spare ribs proident velit esse strip steak buffalo boudin.  Ex ad eiusmod, cupidatat pariatur incididunt hamburger.")
+  const [text, setText] = useState("Tail dolor nisi, culpa proident in turducken ea pork chop adipisicing.  Sint boudin ut burgdoggen sausage deserunt eu beef ribs.  Enim aliqua ullamco quis.  Eu bacon chicken boudin, sunt in kevin ut pariatur excepteur.  Salami cupidatat ad, rump pork belly filet mignon pork loin spare ribs proident velit esse strip steak buffalo boudin.  Ex ad eiusmod, cupidatat pariatur incididunt hamburger.");
+  // const [text, setText] = useState("Tail dolor nisi, culpa proident in turducken ea pork chop adipisicing.  Sint boudin ut burgdoggen sausage deserunt eu beef ribs.  Enim aliqua ullamco quis.  Eu bacon chicken boudin, sunt in kevin ut pariatur excepteur.  Salami cupidatat ad, rump pork belly filet mignon pork loin spare ribs proident velit esse strip steak buffalo boudin.  Ex ad eiusmod, cupidatat pariatur incididunt hamburger.")
+
   const [showText, setShowText] = useState(text.split('').map((el, i) => {
     if (i === 0){
       return (<span className='letter__green'>{el}</span>)
@@ -12,38 +14,47 @@ function App() {
       return (<span className='letter'>{el}</span>)
     }
   }));
+  const [scrollInput, setScrollInput] = useState(0)
   const [isErorr, setIsErorr] = useState(false);
   function changeHandler(e){
-    let printText = e.target.value;
-    let lenText = printText.length;
-    if(text.substring(0, lenText) === printText){
-      setIsErorr(false);
+    console.log(scrollInput);
+    if ((e.target.scrollTop - scrollInput) > 10 ){
+      e.target.scrollTop += 50;
+      setScrollInput(e.target.scrollTop);
+    }
+    let printedText = e.target.value;
+    let lenText = printedText.length;
+    if(text.substring(0, lenText) === printedText){
+      if (isErorr) setIsErorr(false);
       setTapingText(e.target.value);
-      setShowText(text.split('').map((el, i) => {
-        if (i < lenText){
-          return (<span className='letter__print'>{el}</span>)
-        }
-        if (i == lenText){
-          return (<span className='letter__green'>{el}</span>)
-        }
-        return (<span className='letter'>{el}</span>)}))  
+      let newText = wrapSpanText(lenText);
+      setShowText(newText) 
     }else{
       setIsErorr(true);
       setShowText(prevText => {
         prevText[lenText - 1] = (<span className='letter__green error'>{text[lenText - 1]}</span>)
         return prevText
     })
-
     }
+  }
+  function wrapSpanText(lenText = tapingText.length, isErorr = false){
+    return text.split('').map((el, i) => {
+      if (i < lenText){
+        return (<span className='letter__print'>{el}</span>)
+      }
+      if (i === lenText){
+        return (<span className='letter__green'>{el}</span>)
+      }
+      return (<span className='letter'>{el}</span>)})
   }
  
 
   return (
     <div className="App">
-      <div className='container__text' >
+      <div className='container__text' style={{}}>
         {showText}
       </div>
-      <input onChange={changeHandler} value={tapingText} className={isErorr ? 'red input' : 'input'} ></input>
+      <textarea onChange={changeHandler} value={tapingText} className={isErorr ? 'red input' : 'input'} ></textarea>
    
     </div>
   );
